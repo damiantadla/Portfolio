53ea2b923be53eb8bed89285109366c20502e2c5
damiantadla <157707788+damiantadla@users.noreply.github.com>
2024-03-14 11:50:18 +0100

    Add send function to MessengerComponent.vue

--- src/components/MessengerComponent.vue
+++ src/components/MessengerComponent.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { ref } from 'vue'
-
-import InputComponent from '@/components/InputComponent.vue'
+import { ref, reactive } from 'vue'
+import { sendMessage } from '../services/messengerServices.js'
+import { toast } from 'vue3-toastify'
 
 const visibleButton = ref(false)
 const visibleMessenger = ref(false)
+const loading = ref(false)
 
 setTimeout(function () {
   visibleButton.value = true
@@ -12,12 +13,41 @@ setTimeout(function () {
 
 const showMessenger = () => {
   visibleMessenger.value = !visibleMessenger.value
+  messengerData.title = ''
+  messengerData.message = ''
+  messengerData.email = ''
+}
+
+const messengerData = reactive({
+  email: '',
+  title: '',
+  message: ''
+})
+
+const send = async () => {
+  try {
+    loading.value = true
+    if (messengerData.email && messengerData.title && messengerData.message) {
+      await sendMessage(messengerData.email, messengerData.title, messengerData.message)
+      toast.success('Wysłano wiadomość')
+      messengerData.email = ''
+      messengerData.title = ''
+      messengerData.message = ''
+    } else {
+      toast.info('Wypełnij wszystkie pola')
+    }
+  } catch (error) {
+    toast.error(`${error.response.data.message}`)
+  } finally {
+    loading.value = false
+  }
 }
 </script>
 
 <template>
   <div ref="componentMessenger" class="relative z-100">
     <div
+      v-loading="loading"
       v-if="visibleMessenger"
       class="bgBlur fixed z-100 text-center w-[300px] h-[480px] bottom-22 right-5 rounded-xl"
       :class="{ 'animate-startAnim': visibleMessenger, 'animate-exitAnim': !visibleMessenger }"
@@ -27,13 +57,28 @@ const showMessenger = () => {
           Wypełnij formularz, a ja odezwę się tak szybko jak to możliwe...
         </p>
       </div>
-      <InputComponent placeholder="Tytuł" />
-      <InputComponent placeholder="Adres e-mail" />
-      <textarea
-        placeholder="Wiadomość"
-        class="w-[270px] h-32 border-2 placeholder-black my-2 p-4 text-black resize-none rounded-xl"
-      ></textarea>
-      <button class="w-[270px] h-12 rounded-xl bg-amber-400 text-lg font-medium">Wyślij</button>
+      <form @submit.prevent="send">
+        <input
+          v-model="messengerData.email"
+          type="text"
+          placeholder="Email"
+          class="w-[270px] border-2 placeholder-black my-2 py-3 pl-3 text-black rounded-xl"
+        />
+        <input
+          v-model="messengerData.title"
+          type="text"
+          placeholder="Tytuł"
+          class="w-[270px] border-2 placeholder-black my-2 py-3 pl-3 text-black rounded-xl"
+        />
+        <textarea
+          v-model="messengerData.message"
+          placeholder="Wiadomość"
+          class="w-[270px] h-32 border-2 placeholder-black my-2 p-4 text-black resize-none rounded-xl"
+        ></textarea>
+        <button type="submit" class="w-[270px] h-12 rounded-xl bg-amber-400 text-lg font-medium">
+          Wyślij
+        </button>
+      </form>
     </div>
     <!--Button Messenger-->
     <button
