fa0d2039719243f8b0d63e01a503ac118597f5a5
damiantadla <157707788+damiantadla@users.noreply.github.com>
2024-03-14 11:39:38 +0100

    Add new path to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,61 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-
+import DefaultView from '../views/DefaultView.vue'
+import Dashboard from '../views/dashboardAdmin/DashboardAdminView.vue'
+import { useAuthStore } from '../stores/authStore.js'
+import Page404View from '../views/Page404View.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: DefaultView
+  },
+  {
+    path: '/admin',
+    name: 'Dashboard',
+    component: Dashboard,
+    meta: {
+      requiresAuth: true // Ta ścieżka wymaga autentykacji
+    },
+    children: [
+      {
+        path: '', // Usuń początkowy ukośnik
+        name: 'DefaultView',
+        component: () => import('../views/dashboardAdmin/DefaultView.vue') // Dodaj komponent DefaultView
+      },
+      {
+        path: 'messenger', // Usuń początkowy ukośnik
+        name: 'Messenger',
+        component: Page404View // Dodaj komponent Page404View
+      },
+      {
+        path: 'portfolio', // Usuń początkowy ukośnik
+        name: 'Portfolio',
+        component: Page404View // Dodaj komponent Page404View
+      },
+      {
+        path: 'projects', // Usuń początkowy ukośnik
+        name: 'Projects',
+        component: Page404View // Dodaj komponent Page404View
+      }
+    ]
+  },
+  {
+    path: '/:catchAll(.*)', // Obsługa 404
+    component: Page404View // Ustawienie komponentu Page404View dla nieodnalezionych ścieżek
+  }
+]
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-    },
-    {
-      path: '/about',
-      name: 'about',
-    },
-    {
-      path: '/portfolio',
-      name: 'portfolio',
-    },
-    {
-      path: '/contact',
-      name: 'contact',
+  history: createWebHistory(),
+  routes
+})
+
+router.beforeEach((to, from) => {
+  const store = useAuthStore()
+  if (to.meta.requiresAuth && !store.isLoggedIn) {
+    return {
+      path: '/'
     }
-  ]
+  }
 })
 
 export default router
