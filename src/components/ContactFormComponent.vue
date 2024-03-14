7d700fc48b12ad1374706f98a660fd320eaeb5d9
damiantadla <157707788+damiantadla@users.noreply.github.com>
2024-03-14 11:46:25 +0100

    Add to ContactFormComponent.vue send message feature

--- src/components/ContactFormComponent.vue
+++ src/components/ContactFormComponent.vue
@@ -1,31 +1,65 @@
 <script setup>
-import ButtonComponent from '@/components/ButtonComponent.vue'
+import { ref, reactive } from 'vue'
+import { sendMessage } from '../services/messengerServices.js'
+import { toast } from 'vue3-toastify'
+import ButtonComponent from '../components/ButtonComponent.vue'
+
+const data = reactive({
+  email: '',
+  title: '',
+  message: ''
+})
+
+const loading = ref(false)
+
+const send = async () => {
+  try {
+    loading.value = true
+    if (data.email && data.title && data.message) {
+      await sendMessage(data.email, data.title, data.message)
+      toast.success('Wysłano wiadomość')
+      data.email = ''
+      data.title = ''
+      data.message = ''
+    } else {
+      toast.error('Wypełnij wszystkie pola')
+    }
+  } catch (error) {
+    console.log(error)
+    toast.error(error.response.data.message)
+  } finally {
+    loading.value = false
+  }
+}
 </script>
 
 <template>
-  <div class="flex items-center justify-center">
+  <div class="flex items-center justify-center" v-loading="loading">
     <div class="flex items-center max-w-[400px] h-[540px] bg-lightGray mt-20">
-      <div class="flex flex-wrap justify-center">
+      <form class="flex flex-wrap justify-center text-black" @submit.prevent="send">
         <input
+          v-model="data.email"
           class="w-80 h-14 mt-8 pl-4 border-b-4 border-black placeholder-black"
           placeholder="Twój email"
           type="text"
         />
         <input
+          v-model="data.title"
           class="w-80 h-14 mt-8 pl-4 border-b-4 border-black placeholder-black"
           placeholder="Tytuł"
           type="text"
         />
         <textarea
+          v-model="data.message"
           class="w-80 h-50 mt-8 pl-4 py-4 border-b-4 border-black placeholder-black"
-          placeholder="W czyn mogę pomóc?"
+          placeholder="W czym mogę pomóc?"
           name=""
           id=""
           cols="30"
           rows="10"
         ></textarea>
-        <ButtonComponent class="mt-8" text="Wyślij" />
-      </div>
+        <ButtonComponent type="submit" class="mt-8" text="Wyślij" />
+      </form>
     </div>
   </div>
 </template>
